0843e4e60caa680b7d3e43e855a5976821332970
KRS999 <31311195+KRS999@users.noreply.github.com>
2018-07-27 03:14:36 +0300

    Add files via upload

--- js/base.js
+++ js/base.js
@@ -1,87 +1,85 @@
 var workBase = [
 	{
 		img: 'img/site-1.jpg',
-		alt: 'site-1',
 		href: 'KRS999.github.io/site-1/'
 	},
 	{
 		img: 'img/site-2.jpg',
-		alt: 'site-2',
 		href: 'KRS999.github.io/site-2/'
 	},
 	{
 		img: 'img/site-3.jpg',
-		alt: 'site-3',
 		href: 'KRS999.github.io/site-3/'
 	},
 	{
 		img: 'img/site-4.jpg',
-		alt: 'site-4',
 		href: 'KRS999.github.io/site-4/'
 	},
 	{
 		img: 'img/site-5.jpg',
-		alt: 'site-5',
 		href: 'KRS999.github.io/site-5/'
 	},
 	{
 		img: 'img/site-6.jpg',
-		alt: 'site-6',
 		href: 'KRS999.github.io/site-6/'
 	},
 	{
 		img: 'img/site-7.jpg',
-		alt: 'site-7',
 		href: 'KRS999.github.io/site-7/'
 	},
 	{
 		img: 'img/site-8.jpg',
-		alt: 'site-8',
 		href: 'KRS999.github.io/site-8/'
 	},
 	{
 		img: 'img/site-9.jpg',
-		alt: 'site-9',
 		href: 'KRS999.github.io/site-9/'
 	},
 	{
 		img: 'img/site-10.jpg',
-		alt: 'site-10',
 		href: 'KRS999.github.io/site-10/'
 	},
 	{
 		img: 'img/site-11.jpg',
-		alt: 'site-11',
 		href: 'KRS999.github.io/site-11/'
 	},
 	{
 		img: 'img/site-12.jpg',
-		alt: 'site-12',
 		href: 'KRS999.github.io/site-12/'
 	},
 	{
 		img: 'img/site-13.jpg',
-		alt: 'site-13',
 		href: 'KRS999.github.io/site-13/'
 	},
 	{
 		img: 'img/site-14.jpg',
-		alt: 'site-14',
 		href: 'KRS999.github.io/site-14/'
 	},
 	{
 		img: 'img/site-15.jpg',
-		alt: 'site-15',
 		href: 'KRS999.github.io/site-15/'
 	},
 	{
 		img: 'img/site-16.jpg',
-		alt: 'site-16',
 		href: 'KRS999.github.io/site-16/'
 	},
 	{
 		img: 'img/site-17.jpg',
-		alt: 'site-17',
 		href: 'KRS999.github.io/site-17/'
+	},
+	{
+		img: 'img/site-18.jpg',
+		href: 'KRS999.github.io/site-18/'
+	},
+	{
+		img: 'img/site-19.jpg',
+		href: 'KRS999.github.io/site-19/'
+	},
+];
+
+var projectBase = [
+	{
+		img: 'img/site-1.jpg',
+		href: 'KRS999.github.io/site-1/'
 	},
 ];
